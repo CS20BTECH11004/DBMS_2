87b2d4111528a2a58db9fe3c4b85bc27fbe6077d
CS20BTECH11004 <cs20btech11004@iith.ac.in>
2022-02-28 18:58:16 +0530

    fixed sql queries

--- loader.py
+++ loader.py
@@ -1,61 +1,76 @@
+from multiprocessing.dummy import connection
 import psycopg2
 from sympy import true
 import parser
-from sh import pg_dump
 
 user = "postgres"
-password = "amandapanda"
+password = "postgres"
+db_name = "newer_db"
 
-db_connection = psycopg2.connect(
-    database="postgres",
-    user='postgres',
-    password='postgres',
-    host='localhost',
-    port= '5432')
-db_connection.autocommit = true
-cursor = db_connection.cursor()
+def create_db():
+    temp_connection = psycopg2.connect(
+        database='postgres',
+        user=user,
+        password=password,
+        host='localhost',
+        port= '5432')
+    temp_connection.autocommit = true
+    temp_cursor = temp_connection.cursor()
+
+    temp_cursor.execute("DROP DATABASE IF EXISTS "+db_name+";")
+    temp_cursor.execute("CREATE DATABASE "+db_name+";")
+    temp_cursor.close()
+    temp_connection.close()
+def create_tables():
+    db_connection = psycopg2.connect(
+        database=db_name,
+        user=user,
+        password=password,
+        host='localhost',
+        port= '5432')
+    db_connection.autocommit = true
+    cursor = db_connection.cursor()
+    create_table_commands = [
+        """
+        CREATE TABLE research_paper(
+            paper_id VARCHAR(10) UNIQUE NOT NULL,
+            paper_title VARCHAR(255) NOT NULL,
+            abstract VARCHAR(1000) NOT NULL,
+            venue VARCHAR(255) NOT NULL,
+            author_id_index VARCHAR(10) UNIQUE NOT NULL,
+            year INT NOT NULL,
+            PRIMARY KEY(paper_id, paper_title,abstract,venue,author_id_index)
+        );
+        """,
+        """
+        CREATE TABLE reference_table(
+            paper_id VARCHAR(10) NOT NULL references research_paper(paper_id),
+            paper_referenced VARCHAR(10) NOT NULL CHECK(paper_referenced != paper_id),
+            PRIMARY KEY(paper_id, paper_referenced)
+        );
+        """,
+        """
+        CREATE TABLE author_group(
+            author_id_index VARCHAR(10) NOT NULL references research_paper(author_id_index),
+            author_id VARCHAR(10) NOT NULL,
+            author_rank INT NOT NULL,
+            PRIMARY KEY(author_id_index,author_id)
+        )
+        """,
+        """
+        CREATE TABLE author_info(
+            author_id VARCHAR(10) PRIMARY KEY NOT NULL,
+            first_name VARCHAR(50) NOT NULL,
+            second_name VARCHAR(50) NOT NULL 
+        )
+        """
+    ]
+    for command in create_table_commands:
+        cursor.execute(command)
+
+    cursor.close()
+    db_connection.close()
 
 
-cursor.execute("DROP DATABASE testdb")
-cursor.execute("CREATE DATABASE testdb")
-#create tables
-create_table_commands = [
-    """
-    CREATE TABLE research_paper(
-        paper_id VARCHAR(10) NOT NULL,
-        paper_title VARCHAR(255) NOT NULL,
-        abstract VARCHAR(1000) NOT NULL,
-        venue VARCHAR(255) NOT NULL,
-        author_id_index VARCHAR(10) NOT NULL,
-        year INT NOT NULL,
-        PRIMARY KEY(paper_id, paper_title,abstract,venue,author_ids)
-    );
-    """,
-    """
-    CREATE TABLE reference_table(
-        paper_id VARCHAR(10) NOT NULL references research_paper(paper_id),
-        paper_referenced VARCHAR(10) NOT NULL CHECK(paper_referenced != paper_id)
-        PRIMARY KEY(paper_id, paper_referenced)
-    );
-    """,
-    """
-    CREATE TABLE author_group(
-        author_id_index VARCHAR(10) NOT NULL references research_paper(author_id_index),
-        author_id VARCHAR(10) NOT NULL,
-        author_rank INT NOT NULL
-        PRIMARY KEY(author_id_index,author_id)
-    )
-    """,
-    """
-    CREATE TABLE author_info(
-        author_id VARCHAR(10) NOT NULL
-        first_name VARCHAR(50) NOT NULL
-        second_name VARCHAR(50) NOT NULL 
-    )
-    """,
-
-]
-for command in create_table_commands:
-    cursor.execute(command)
-
-db_connection.close()
+create_db()  
+create_tables()
