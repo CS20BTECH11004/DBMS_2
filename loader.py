94eab5b13b94887b1f6d8335927664ecb259fc6c
CS20BTECH11004 <cs20btech11004@iith.ac.in>
2022-03-02 13:35:48 +0530

    completed basic working of loader

--- loader.py
+++ loader.py
@@ -3,12 +3,15 @@ import psycopg2
 from sympy import true
 import parser
 import time
+#import pyodbc
 
 user = "postgres"
 password = "postgres"
 db_name = "newer_db"
-
+file_info = []
 def create_db():
+    global user
+    global password
     temp_connection = psycopg2.connect(
         database='postgres',
         user=user,
@@ -19,24 +22,26 @@ def create_db():
     temp_cursor = temp_connection.cursor()
 
     temp_cursor.execute("DROP DATABASE IF EXISTS "+db_name+";")
-    temp_cursor.execute("CREATE DATABASE "+db_name+";")
+    temp_cursor.execute("CREATE DATABASE "+db_name+" ENCODING = 'UTF8';")
     temp_cursor.close()
     temp_connection.close()
 def create_tables():
+    global user
+    global password
     db_connection = psycopg2.connect(
         database=db_name,
         user=user,
         password=password,
         host='localhost',
         port= '5432')
-    db_connection.autocommit = true
+    db_connection.set_client_encoding('UTF-8')
     cursor = db_connection.cursor()
     create_table_commands = [
         """
         CREATE TABLE research_paper(
             paper_id VARCHAR(10) UNIQUE NOT NULL,
             paper_title VARCHAR(500) NOT NULL,
-            abstract VARCHAR(1000) NOT NULL,
+            abstract TEXT NOT NULL,
             venue VARCHAR(500) NOT NULL,
             year VARCHAR(100) NOT NULL,
             PRIMARY KEY(paper_id, paper_title,venue)
@@ -61,71 +66,118 @@ def create_tables():
         CREATE TABLE author_info(
             author_id VARCHAR(10) PRIMARY KEY NOT NULL,
             first_name VARCHAR(50) NOT NULL,
-            second_name VARCHAR(50) NOT NULL 
+            middle_name VARCHAR(50),
+            last_name VARCHAR(50) NOT NULL 
         )
         """
     ]
     for command in create_table_commands:
         cursor.execute(command)
-
+    db_connection.commit()
     cursor.close()
     db_connection.close()
+def get_parsed_data():
+    global file_info
+    print("reading from file")
+    file_info = parser.get_paper_info()
 def input_into_db():
+    global file_info
     db_connection = psycopg2.connect(
         database=db_name,
         user=user,
         password=password,
         host='localhost',
         port= '5432')
-    db_connection.autocommit = true
+    #db_connection.set_client_encoding('UTF-8')
+    
+    #db_connection.autocommit = true #too damn slow
     cursor = db_connection.cursor()
-    paper_count = parser.paper_count
-    while paper_count > 0:
-        (title,author,year,venue,paper_id,references,abstract) = parser.get_paper_info()
-        if title == "":
-            title = "UNAVAILABLE"
-        if author == "":
-            author = "UNAVAILABLE"
-        if year == 0:
-            year = "UNAVAILABLE"
-        if venue == "":
-            venue = "UNAVAILABLE"
-        if abstract == "":
-            abstract = "UNAVAILABLE"
-        
-        
-        # #inserting into research_paper
-        # sql="""INSERT INTO research_paper(
-        #     paper_id,
-        #     paper_title,
-        #     abstract,
-        #     venue,
-        #     year
-        #     )
-        #     VALUES("""+"'"+paper_id+"',"+"'"+title+"',"+"'"+abstract+"',"+"'"+venue+"','"+str(year)+"');"
-        # cursor.execute(sql)
+    cursor.execute("SET CLIENT_ENCODING TO 'UTF-8';")
+    db_connection.commit()
+    #cursor.fast_executemany =true
+    
+    #entering into research_paper
+    resch_paper_sql = """
+    INSERT INTO research_paper(
+        paper_id,
+        paper_title,
+        abstract,
+        venue,
+        year
+    )
+    VALUES(%s,%s,%s,%s,%s)"""
+    resch_paper_values = []
+    for x in file_info:
+        resch_paper_values.append((x[4],x[0],x[6],x[3],str(x[2])))
+    cursor.executemany(resch_paper_sql,resch_paper_values)
+    
+    #entering into reftable
+    ref_table_sql = """
+    INSERT INTO reference_table(paper_id, paper_referenced) 
+    VALUES(%s,%s)"""
+    ref_table_vals = []
+    for x in file_info:
+        for y in x[5]:
+            ref_table_vals.append((x[4],y))
 
-        # #inserting into reference table
-        # for ref_paper in references:
-        #     cursor.execute("""
-        #     INSERT INTO reference_table(paper_id, paper_referenced)
-        #     VALUES(%s,%s)""",((paper_id,ref_paper),))
-        try:
-            sql = """INSERT INTO reference_table(paper_id, paper_referenced) 
-            VALUES(%s,%s)"""
-            cursor.executemany(sql,)
-            db_connection.commit()
-        except:
-            db_connection.rollback()
-        paper_count -=1
+
+    cursor.executemany(ref_table_sql,ref_table_vals)
+    #entering into author_info
+    auth_info_sql = """
+    INSERT INTO author_info(
+        author_id,
+        first_name,
+        middle_name,
+        last_name
+    )VALUES(%s,%s,%s,%s);"""
+    auth_info_values = []
+    ttt = 69 #ik im shitting my pants here but bear with me
+    for x in file_info:
+        for i in range(len(x[1])):
+            fname= x[1][i].split(' ')[0]
+            lastname = x[1][i].split(' ')[-1]
+            middlename = ""
+            if(len(x[1][i].split(' '))>2):
+                middlename = x[1][i].split(' ')[1]
+            auth_info_values.append((ttt,fname,middlename,lastname))
+            ttt+=1
+    cursor.executemany(auth_info_sql,auth_info_values)
+
+    #entering into author_group
+    auth_grp_sql = """
+    INSERT INTO author_group(paper_id, author_id, author_rank) 
+    VALUES(%s,%s,%s);"""
+    auth_grp_vals = []
+    for x in file_info:
+        for i in range(len(x[1])):
+            fname= x[1][i].split(' ')[0]
+            lastname = x[1][i].split(' ')[-1]
+            middlename = ""
+            if(len(x[1][i].split(' '))>2):
+                middlename = x[1][i].split(' ')[1]
+            auth_id =""
+            for t in auth_info_values:
+                if t[1]==fname and t[2]==middlename and t[3]==lastname:
+                    auth_id = t[0]
+            auth_grp_vals.append((x[4],str(auth_id),str(i+1)))
+    cursor.executemany(auth_grp_sql,auth_grp_vals)
+
+    db_connection.commit()
+    # try:
+        
+    # except:
+    #     print("error occured.")
+    #     db_connection.rollback()
+    db_connection.close()
 def load():
-    start_time =time.time()
-    print("loading started")
+    print("inserting into db")
     input_into_db()
-    end_time = time.time()
-    print("loading ended")
-    print("Time taken: "+str(end_time-start_time)+" seconds")
+    print("finished task")
 
 create_db()  
 create_tables()
+start_time =time.time()
+get_parsed_data()
 load()
+end_time = time.time()
+print("Time taken: "+str(end_time-start_time)+" seconds")
--- parser.py
+++ parser.py
@@ -5,33 +5,47 @@ file = open("source.txt","r", encoding="utf8")
 paper_count = int(file.readline())
 
 def get_paper_info():
-    global file
+    global file 
+    file_info =[]
+    for i in range(paper_count):
+        title = ""
+        author= []
+        year = ""
+        venue = ""
+        paper_id = ""
+        references =[]
+        abstract = ""
+
+        file_inp = file.readline().strip() 
+        while(file_inp!=""):
+            if file_inp[0:2] == '#*':
+                title = file_inp[2:].replace("'","''")
+            elif file_inp[0:2] == '#@':
+                author = file_inp[2:].replace("'","''").split(',')
+            elif file_inp[0:2] == '#t':
+                year = file_inp[2:]
+            elif file_inp[0:2] == '#c':
+                venue = file_inp[2:].replace("'","''")
+            elif file_inp[0:6] == '#index':
+                paper_id = file_inp[6:].replace("'","''")
+            elif file_inp[0:2] == '#%':
+                references.append(file_inp[2:].replace("'","''"))
+            elif file_inp[0:2] == '#!':
+                abstract = file_inp[2:].replace("'","''")
+            
+            file_inp = file.readline().strip()
+        
+        if title == "":
+            title = "UNAVAILABLE"
+        if author == "":
+            author = "UNAVAILABLE"
+        if year == "":
+            year = "UNAVAILABLE"
+        if venue == "":
+            venue = "UNAVAILABLE"
+        if abstract == "":
+            abstract = "UNAVAILABLE"
+        
+        file_info.append((title,author,year,venue,paper_id,references,abstract))
     
-    title = ""
-    author= []
-    year = int()
-    venue = ""
-    paper_id = ""
-    references =[]
-    abstract = ""
-
-    file_inp = file.readline().strip() 
-    while(file_inp!=""):
-        if file_inp[0:2] == '#*':
-            title = file_inp[2:].replace("'","''")
-        elif file_inp[0:2] == '#@':
-            author = file_inp[2:].replace("'","''").split(',')
-        elif file_inp[0:2] == '#t':
-            year = int(file_inp[2:])
-        elif file_inp[0:2] == '#c':
-            venue = file_inp[2:0].replace("'","''")
-        elif file_inp[0:6] == '#index':
-            paper_id = file_inp[6:].replace("'","''")
-        elif file_inp[0:2] == '#%':
-            references.append(file_inp[2:].replace("'","''"))
-        elif file_inp[0:2] == '#!':
-            abstract = file_inp[2:0].replace("'","''")
-
-        file_inp = file.readline().strip()
-
-    return (title,author,year,venue,paper_id,references,abstract)
+    return file_info
